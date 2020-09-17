cb3ef52588e1077b0dd654afa0c913617629cd0d
Rafael <38105703+MRxRafi@users.noreply.github.com>
2020-09-17 10:39:05 +0200

    Feature 2 (#2)
    * Añadidos cambios
    
    * Añadidos cambios2
    
    * Añadidos cambios3

--- src/main/java/es/upm/miw/iwvg_devops/ForkTest.java
+++ src/main/java/es/upm/miw/iwvg_devops/ForkTest.java
@@ -1,2 +1,12 @@
-package es.upm.miw.iwvg_devops;public class ForkTest {
+package es.upm.miw.iwvg_devops;
+public class ForkTest {
+    int hola;
+
+    public ForkTest(){
+        hola = 3;
+    }
+
+    public int getHola(){
+        return hola;
+    }
 }
